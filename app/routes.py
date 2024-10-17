c0034ccd88bc78dfb85b50b54f60cae0e0627cf8
ibaragozz <baragozz1@gmail.com>
2024-10-17 20:00:15 +0300

    HW28done

--- app/routes.py
+++ app/routes.py
@@ -1,16 +1,16 @@
-from flask import render_template, request, redirect, url_for
+from flask import render_template, request
 from app import app
 
 posts = []
 
-
 @app.route('/', methods=['GET', 'POST'])
-
 def index():
     if request.method == 'POST':
-        title = request.form.get('title')
-        content = request.form.get('content')
-        if title and content:
-            posts.append({'title': title, 'content': content})
-            return redirect(url_for('index'))
+        name = request.form.get('name')
+        city = request.form.get('city')
+        hobby = request.form.get('hobby')
+        age = request.form.get('age')
+        if name and city and hobby and age:
+            posts.append({'name': name, 'city': city, 'hobby': hobby, 'age': age})
+
     return render_template('blog.html', posts=posts)
