001262d4b5e85db46125cc05d24fab657d4fdfb6
ibaragozz <baragozz1@gmail.com>
2024-10-17 19:08:28 +0300

    lessonDone

--- app/routes.py
+++ app/routes.py
@@ -7,4 +7,10 @@ posts = []
 @app.route('/', methods=['GET', 'POST'])
 
 def index():
-    
+    if request.method == 'POST':
+        title = request.form.get('title')
+        content = request.form.get('content')
+        if title and content:
+            posts.append({'title': title, 'content': content})
+            return redirect(url_for('index'))
+    return render_template('blog.html', posts=posts)
